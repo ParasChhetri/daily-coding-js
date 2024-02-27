2ec87c43a1a930f27bfbd723c50c27101309aab4
ParasChhetri <paraschhetri477@gmail.com>
2024-02-27 18:28:08 +0530

    Some new concepts and new questions added

--- day-02/script.js
+++ day-02/script.js
@@ -27,7 +27,33 @@ const firstWordLength = (s) => {
 
 console.log(firstWordLength("     abc hrlo "));
 
+
 // Q.3 SOLUTION
+const middleWorlLength = (s) => {
+    if (s.length === 0) {
+        return 0;
+    }
+    else {
+        const word = s.trim().split(' ');
+        const index = Math.trunc(word.length / 2);
+        const getWord = word[index];
+        console.log(getWord);
+        return word[index].length;
+    }
+}
+
+console.log(middleWorlLength(" apple is jac apple agscugu ahfiuwehghe sdbvibd akjbfueg"));
+
+// A WAY OF DOING THE ADDITION AND SUBSTRACTION OR ANY OTHER OPRATIONS TOGEATHER
+const addMinus = (a,b) => {
+   let sum = a + b;
+   let minus = a - b;
+   return [sum, minus];
+}
+
+console.log(addMinus(12,90));
+
+// Q.4 SOLUTION
 const isIsomorphic = (s,t) => {
     let mapS = {}, mapT = {};
     for (let i = 0; i < s.length; i++) {
