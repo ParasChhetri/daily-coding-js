6189ca2ded3e7f9b879ea15afb25b1244e8f9aeb
ParasChhetri <paraschhetri477@gmail.com>
2024-03-01 23:07:08 +0530

    day 04

---
+++ day-04/script.js
@@ -0,0 +1,42 @@
+// Q.1 Solution
+const currentDayTime = () => {
+    let today = new Date();
+    let day = today.getDay();
+    console.log(day);
+    let dayNames = ["Sunday","Monday","Tuseday","Wednesday","Thursday","Friday","Saturday"];
+    let currentDayName = dayNames[day];
+    console.log(currentDayName);
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+    let timeFormat = (hour >= 12) ? "PM" : "AM";
+    hour = (hour >= 12) ? hour - 12: hour;
+    // if (hour === 0 && timeFormat === "PM") {
+    //     if (minute === 0 && second === 0) {
+    //         hour = 12;
+    //         timeFormat = "Midnight";
+    //     }
+    //     else {
+    //         hour = 12;
+    //         timeFormat = "PM";
+    //     }
+    // }
+
+    // if (hour === 0 && timeFormat === "AM") {
+    //     if (minute === 0 && second === 0) {
+    //         hour = 12;
+    //         timeFormat = "Midnight";
+    //     }
+    //     else {
+    //         hour = 12;
+    //         timeFormat = "AM";
+    //     }
+    // }
+    let today_day_time = `Today day is: ${currentDayName}\nCurrent time is: ${hour} : ${timeFormat} : ${minute} : ${second}`;
+    return console.log(today_day_time);
+}
+
+currentDayTime();
+
+// Q.2 Solution
+const printCurrentPage = () => window.print();
