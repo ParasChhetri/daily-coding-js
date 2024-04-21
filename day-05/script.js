7c4dcec15b8259a62cac26db19ecde1958bc33c2
ParasChhetri <paraschhetri477@gmail.com>
2024-04-21 12:51:27 +0530

    day - complete

---
+++ day-05/script.js
@@ -0,0 +1,189 @@
+// Q.1 Solution
+function checkString(s) {
+    if (typeof s === 'string') {
+        return console.log('It is a string');
+    }
+    else {
+        return console.log('No its not a string');
+    }
+}
+
+checkNumber("apple");
+checkNumber(12345);
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+
+// Q.2 Solution
+function checkNumber(n) {
+    if (typeof n === 'number') {
+        return console.log(true);
+    }
+    else {
+        return console.log(`${typeof(n)}, because the value passed was`, n);
+    }
+}
+checkNumber("apple");
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e13);
+checkNumber(12345);
+checkNumber("apple");
+checkNumber([1,2,3,4,3,444,555,6667,10202,88,8890]);
+checkNumber("apple");
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber({name: "paras",age : 12, working: false, school : false, college : false, games : true});
+checkNumber("apple");
+checkNumber(12345e10);
+checkNumber(12345e14);
+checkNumber(12345e10);
+checkNumber(12345e14);
+
+
+// Q.3 Part 1
+function findMe() {
+    a = 10;
+    b = 10;
+    c = 10;
+    if (a == b == c) {
+        return console.log("I am running");
+    }
+    else {
+        return console.log("sorry");
+    }
+}
+
+findMe();
+
+// Q.3 Part 2
+function findMeTwo() {
+    a = 10;
+    b = 10;
+    c = 10;
+    if (a === b === c) {
+        return console.log("I am running");
+    }
+    else {
+        return console.log("sorry");
+    }
+}
+
+findMeTwo();
